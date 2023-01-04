d048ab8c8296b3fcd8c964412d5618ffa53f4e37
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-04 20:20:31 +0100

    e

--- website/src/app.rs
+++ website/src/app.rs
@@ -4,9 +4,7 @@ use yew::prelude::*;
 pub fn app() -> Html {
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+<a>{"hello from rust"}</a>
         </main>
     }
 }
