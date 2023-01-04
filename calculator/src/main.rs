ed06a44d62745a9bc639344e56354f7b6df35517
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-04 16:40:36 +0100

    not working

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -2,12 +2,23 @@ use std::env::{args, Args};
 fn main() {
 let mut args: Args= args();
 let first = args.nth(1).unwrap();
-let operator = args.nth(2).unwrap();
-let second = args.nth(3).unwrap();
+let operator = args.nth(0).unwrap();
+let second = args.nth(0).unwrap();
+let first_number: f32 = first.parse().unwrap();
+let second_number: f32 = second.parse().unwrap();
 println!("{:?} {} {}", first, operator, second);
+
 }
-fn nth(&mut self, n: usize) -> Option<String>{
-    self.inner.next();
-    self.inner.next()
-    
+fn operator(operator: char, first_number: f32, second_number: f32) -> f32{
+if operator == '+' {
+    first_number + second_number
+}else if operator == '-' {
+first_number - second_number
+}else if operator == '/' {
+    first_number / second_number
+} else if operator == '*' {
+    first_number * second_number
+} else {
+    0.0
+}
 }
