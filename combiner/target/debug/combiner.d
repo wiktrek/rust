b8b6266bb7e4d5c2ac222791746d2ff8ece0365d
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-04 20:18:38 +0100

    YEW WORKS

--- combiner/target/debug/combiner.d
+++
@@ -1,1 +0,0 @@
-D:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\combiner.exe: D:\Users\wiktor\Desktop\projekty\rust\combiner\src\args.rs D:\Users\wiktor\Desktop\projekty\rust\combiner\src\main.rs
--- combiner/target/debug/deps/combiner-97ceeff3ef3d223f.d
+++
@@ -1,6 +0,0 @@
-d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-97ceeff3ef3d223f.rmeta: src\main.rs src\args.rs
-
-d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-97ceeff3ef3d223f.d: src\main.rs src\args.rs
-
-src\main.rs:
-src\args.rs:
