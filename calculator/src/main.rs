9d5d24382a68e933dfcb041ed7f79def8f1cbc6d
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-04 16:54:16 +0100

    WORKS

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,24 +1,25 @@
 use std::env::{args, Args};
 fn main() {
 let mut args: Args= args();
-let first = args.nth(1).unwrap();
-let operator = args.nth(0).unwrap();
-let second = args.nth(0).unwrap();
+let first: String = args.nth(1).unwrap();
+let operator: char = args.nth(0).unwrap().chars().next().unwrap();
+let second: String = args.nth(0).unwrap();
 let first_number: f32 = first.parse().unwrap();
 let second_number: f32 = second.parse().unwrap();
-println!("{:?} {} {}", first, operator, second);
+let result: f32 = operate(operator, first_number, second_number);
+println!("{:?}", output(first_number, second_number, operator, result))
 
 }
-fn operator(operator: char, first_number: f32, second_number: f32) -> f32{
-if operator == '+' {
-    first_number + second_number
-}else if operator == '-' {
-first_number - second_number
-}else if operator == '/' {
-    first_number / second_number
-} else if operator == '*' {
-    first_number * second_number
-} else {
-    0.0
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32{
+match operator {
+    '+' => first_number + second_number,
+    '-' => first_number - second_number,
+    '*' => first_number * second_number,
+    '/' => first_number / second_number,
+    _ => panic!("Invalid operator used.")
 }
+}
+
+fn output(first_number: f32, second_number: f32, operator: char, result: f32) -> String {
+    format!("{} {} {} = {}", first_number, operator, second_number, result)
 }
