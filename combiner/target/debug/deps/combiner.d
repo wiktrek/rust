9980adc38fbca9ab783a46d4fbd25b4e296d3a55
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-04 17:02:04 +0100

    combiner

---
+++ combiner/target/debug/deps/combiner.d
@@ -0,0 +1,6 @@
+D:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner.exe: src\main.rs src\args.rs
+
+D:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner.d: src\main.rs src\args.rs
+
+src\main.rs:
+src\args.rs:
