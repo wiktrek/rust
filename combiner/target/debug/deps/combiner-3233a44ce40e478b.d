57a8110d0365c0ebdf76f6f57352e0244398e4bf
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-07 23:54:27 +0100

    main.rs import args.rs

--- combiner/target/debug/deps/combiner-3233a44ce40e478b.d
+++ combiner/target/debug/deps/combiner-3233a44ce40e478b.d
@@ -1,5 +1,6 @@
-d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-3233a44ce40e478b.rmeta: src\main.rs
+d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-3233a44ce40e478b.rmeta: src\main.rs src\args.rs
 
-d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-3233a44ce40e478b.d: src\main.rs
+d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-3233a44ce40e478b.d: src\main.rs src\args.rs
 
 src\main.rs:
+src\args.rs:
--- combiner/target/debug/deps/combiner-97ceeff3ef3d223f.d
+++ combiner/target/debug/deps/combiner-97ceeff3ef3d223f.d
@@ -1,5 +1,6 @@
-d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-97ceeff3ef3d223f.rmeta: src\main.rs
+d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-97ceeff3ef3d223f.rmeta: src\main.rs src\args.rs
 
-d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-97ceeff3ef3d223f.d: src\main.rs
+d:\Users\wiktor\Desktop\projekty\rust\combiner\target\debug\deps\combiner-97ceeff3ef3d223f.d: src\main.rs src\args.rs
 
 src\main.rs:
+src\args.rs:
