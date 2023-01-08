a9c95d99caa7013f11c4198223093acdfeb31e5b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-08 18:55:43 +0100

    works

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,10 +1,31 @@
 mod args;
 use args::Args;
-use image::{ io::Reader,DynamicImage, ImageFormat };
+use image::{ io::Reader,DynamicImage, ImageFormat, GenericImageView, imageops::FilterType::Triangle};
 use std::{ io::BufReader, fs::File};
+use std::convert::TryInto;
+#[derive(Debug)]
 enum ImageDataErrors {
 DiffrentImageFormats,
 
+}
+struct FloatingImage {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+    name: String,
+}
+impl FloatingImage {
+    fn new(width: u32, height: u32, name: String) -> Self {
+        let buffer_capacity: u32 = height * width * 4;
+        let buffer: Vec<u8> = Vec::with_capacity(buffer_capacity.try_into().unwrap());
+FloatingImage { 
+    width , 
+    height , 
+    data: buffer, 
+    name  ,
+}
+
+    }
 }
 fn main() -> Result<(), ImageDataErrors> {
     let args = Args::new();
@@ -13,6 +34,8 @@ fn main() -> Result<(), ImageDataErrors> {
 if image_format_1 != image_format_2 {
     return Err(ImageDataErrors::DiffrentImageFormats)
 }
+let (image_1, image_2) = standardise_size(image_1, image_2);
+let ouput = FloatingImage::new(image_1.width(), image_1.height(), args.output,);
 Ok(())
 }
 
@@ -23,5 +46,17 @@ fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
  (image, image_format)
 }
 fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
-    let pix_1 = dim_1.0 * dim_1.1;
+    let pix_1: u32 = dim_1.0 * dim_1.1;
+    let pix_2: u32 = dim_2.0 * dim_2.1;
+    return if pix_1 < pix_2 { dim_1 } else {dim_2}
+}
+
+fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage){
+let (width,height) = get_smallest_dimension(image_1.dimensions(), image_2.dimensions());
+println!("width: {}, height: {}\n", width, height);
+if image_2.dimensions() == (width, height) {
+    (image_1.resize_exact(width, height, Triangle), image_2)
+} else {
+    (image_1, image_2.resize_exact(width, height, Triangle))
+}
 }
