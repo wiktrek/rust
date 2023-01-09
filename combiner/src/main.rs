bb0587406d38a1320b93ed0a5a6abe4303467e2b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-09 18:04:37 +0100

    alternate_pixels function

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -61,5 +61,22 @@ if image_2.dimensions() == (width, height) {
 }
 }
 fn combine_images(image_1: DynamicImage, image_2: DynamicImage,) -> Vec<u8>{
+    let vec_1 = image_1.to_rgb8().into_vec();
+    let vec_2 = image_2.to_rgb8().into_vec();
+    alternate_pixels(vec_1, vec_2)
+}
+fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
+    let combined_data = vec![0u8; vec_1.len()];
+    let mut i = 0;
+    while i < vec_1.len() {
+        if i % 8 == 0 {
+            combined_data.splice(i..=i + 3, set_rgba(vec_1, i, i + 3));
+        } else {
+            combined_data.splice(i..=i +3, set_rgba(vec_1, i, i + 3));
+        }
+        i += 4;
+    }
+}
+fn set_rgba(vec: Vec<u8>, start: usize, end: usize) -> Vec<u8> {
     
 }
