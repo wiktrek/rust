547e089928c508e356ac9a640f79af36a0de0764
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-11 21:21:23 +0100

    leetcode

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,6 +1,8 @@
 mod convert;
 mod valid_parentheses;
 mod two_sum;
+mod palindrome_number;
+use palindrome_number::is_palindrome;
 use convert::convert;
 use two_sum::sum;
 
@@ -9,5 +11,6 @@ sum(vec![1, 2, 3,5], 7);
 println!("  main.rs:");
 println!("Hello World");
 convert("VI");
+is_palindrome(2442);
 valid_parentheses::is_valid("()()()(){}".to_string());
 }
