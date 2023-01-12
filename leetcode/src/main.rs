abc0ec972d475726ecfe428e70d4f2d1101d1357
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-12 21:59:26 +0100

    .

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -3,10 +3,12 @@ mod valid_parentheses;
 mod two_sum;
 mod palindrome_number;
 mod lcp;
+mod generate_parentheses;
 use palindrome_number::is_palindrome;
 use convert::convert;
 use two_sum::sum;
 use lcp::longest_common_prefix;
+use generate_parentheses::generate_parenthesis;
 fn main() {
 sum(vec![1, 2, 3,5], 7);
 println!("  main.rs:");
@@ -15,4 +17,5 @@ convert("VI");
 is_palindrome(2442);
 valid_parentheses::is_valid("()()()(){}".to_string());
 longest_common_prefix(vec!["flower","flow","flight"]);
+generate_parenthesis(2);
 }
