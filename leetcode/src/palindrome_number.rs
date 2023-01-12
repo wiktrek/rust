81785ac18d5d642725815c9defb5f52754a97fe8
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-12 22:00:16 +0100

    .

--- leetcode/src/palindrome_number.rs
+++
@@ -1,6 +0,0 @@
-pub fn is_palindrome(x: i32) -> bool {
-    println!("  palindrome_number.rs:");
-    let is = x.to_string()==x.to_string().chars().rev().collect::<String>();
-    println!("{is}");
-    is
-}
