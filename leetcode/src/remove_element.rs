bfbd71b74170082d48fa2590cbbc0d55b1056458
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-15 21:08:01 +0100

    not working D:

--- leetcode/src/remove_element.rs
+++ leetcode/src/remove_element.rs
@@ -1,11 +1,10 @@
 pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-    let expected: Vec<&i32> = Vec::new();
     nums.sort();
     for n in nums {
         if n != &val {
-expected.push(n);
+nums.remove(n as u32);
         }
 
     }
-        return expected.len() as i32;
+        return nums.len() as i32;
 }
