7e04ade84c6df2e83ee8be8015769575e96e42dd
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-15 21:08:41 +0100

    trying to match types

--- leetcode/src/remove_element.rs
+++ leetcode/src/remove_element.rs
@@ -2,7 +2,7 @@ pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
     nums.sort();
     for n in nums {
         if n != &val {
-nums.remove(n as u32);
+nums.remove(n);
         }
 
     }
