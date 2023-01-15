85b93e9e22b9b3f2de160baa97e4f30b46ec4a6a
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-15 21:23:57 +0100

    search_insert

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,6 +1,9 @@
 mod remove_element;
+mod serach_insert;
 use remove_element::remove_element;
+use serach_insert::search_insert;
 fn main() {
     let nums = &mut vec![1,2,3,4,5];
 remove_element(nums, 4);
+search_insert(vec![1,4,5], 3);
 }
---
+++ leetcode/src/serach_insert.rs
@@ -0,0 +1,4 @@
+pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+    
+        return 1;
+}
