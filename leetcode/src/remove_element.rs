923d643da5993bcba32e76e28c1543d477b016d5
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-15 21:21:17 +0100

    WORKS!

--- leetcode/src/remove_element.rs
+++ leetcode/src/remove_element.rs
@@ -1,10 +1,13 @@
 pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    let mut total = nums.len();
     nums.sort();
-    for n in nums {
-        if n != &val {
-nums.remove(n);
+    for n in (0..total).rev()  {
+        if nums.is_empty() && nums[n] == val {
+            nums.remove(n);
+            total -= 1;
         }
 
     }
-        return nums.len() as i32;
+    println!("{total}");
+        return total as i32;
 }
