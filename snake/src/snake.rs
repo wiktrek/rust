b4d03a562e83cb4581321778121973dbe44430ff
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-20 13:51:29 +0100

    snake.rs

---
+++ snake/src/snake.rs
@@ -0,0 +1,35 @@
+use std::collections::LinkedList;
+use piston_window::{Context, G2d};
+use piston_window::types::Color;
+use draw::draw_block;
+const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0];
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
+struct Block {
+    x: i32,
+    y: i32,
+}
+struct Snake {
+    direction: Direction,
+    body: LinkedList<Block>,
+    tail: Option<Block>
+}
+impl Snake {
+    pub fn new (x: i32, y: i32) -> Snake {
+        
+    }
+}
