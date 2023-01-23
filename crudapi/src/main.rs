b791f3f076a75edb714fde94f158c9f43e23089c
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-23 21:08:24 +0100

    no errors

--- crudapi/src/main.rs
+++ crudapi/src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate rocket;
+use rocket::State;
 use rocket::http::Status;
 use rocket::response::Redirect;
 use rocket::http::uri::Origin;
@@ -7,31 +8,36 @@ use rocket::serde::json::{json, Value};
 use reqwest;
 use reqwest::Client;
 const SEARCH_PREFIX: Origin<'static> = uri!("/search");
+const REPO: &str = "wiktrek/rust";
 #[get("/")]
 fn index() -> Redirect {
     let msg:Option<&str> = None;
     Redirect::to(uri!(SEARCH_PREFIX, search(msg)))
 }
-fn get_latest_release(client: &Client,repo: &str)  -> Result<Value,reqwest::Error> {
+async fn get_latest_release(client: &Client,repo: &str)  -> Result<Value,reqwest::Error> {
     let url = format!("http://api.github.com/repos/{repo}/releases/latest");
     let response = client.get(&url).send().await?;
     let github_release = response.json::<Value>().await?;
-    github_release
+    Ok(github_release)
 }
-#[get("/search?<repo>")]
-fn search(msg: Option<&str>, client: &State<Client>) -> Result<Value, Status> {
+#[get("/search?<msg>")]
+async fn search(msg: Option<&str>, client: &State<Client>) -> Result<Value, Status> {
 
 if let Some(msg) = msg {
-    println!("{msg}");
+    println!("repo");
     return Err(Status::NoContent);
 }
-get_latest_release(, ).await.or_else(Err(Status::NoContent))
+get_latest_release(client, REPO).await.or(Err(Status::NoContent))
 
 }
 #[launch]
 fn rocket() -> _ {
-    rocket::build().menage(
-        reqwest::Client::builder().user_agent("reqwest").build().unwrap()
+    rocket::build()
+        .manage(
+        reqwest::Client::builder()
+        .user_agent("reqwest")
+        .build()
+        .unwrap()
     )
     .mount("/",routes![index])
     .mount(SEARCH_PREFIX,routes![search])
