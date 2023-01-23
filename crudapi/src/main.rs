a20e776f2aa13e2d4c457290f977919ab6624d8c
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-23 20:02:54 +0100

    crudapi

---
+++ crudapi/src/main.rs
@@ -0,0 +1,12 @@
+extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/hello", routes![hello])
+        .launch();
+}
