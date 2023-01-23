19bd7e7b81ba2e1783bf9a4bf2312a7a7f275a26
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-23 20:46:20 +0100

    reqwest

--- crudapi/src/main.rs
+++ crudapi/src/main.rs
@@ -5,13 +5,18 @@ use rocket::http::Status;
 use rocket::response::Redirect;
 use rocket::http::uri::Origin;
 use rocket::serde::json::{json, Value};
+use reqwest::Client;
 const SEARCH_PREFIX: Origin<'static> = uri!("/search");
 #[get("/")]
 fn index() -> Redirect {
     let msg:Option<&str> = None;
     Redirect::to(uri!(SEARCH_PREFIX, search(msg)))
 }
+fn get_latest_release(client: &Client,repo: &str) -> Value {
+    let url = format!("http://api.github.com/repos/{repo}/releases/latest");
+    let response = client.get(&url).send().await?;
 
+}
 #[get("/search?<msg>")]
 fn search(msg: Option<&str>) -> Result<Value, Status> {
 
