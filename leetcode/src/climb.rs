47dbbdf58ba82e2f7a49e1f6759234ba28690784
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-24 20:20:40 +0100

    leetcode

--- leetcode/src/climb.rs
+++ leetcode/src/climb.rs
@@ -2,7 +2,17 @@ pub fn climb(n: i32) -> i32 {
 let mut solutions: i32 = 0;
 let mut one_step_before = 2;
 let mut two_steps_before = 1;
-for _ in 0..n {
+if n <= 0 {
+    return 0;
+}
+if n == 1 {
+    return 1
+}
+if n == 2 { 
+    return 2; 
+}
+
+for _ in 2..n {
     println!("{solutions}, {one_step_before}, {two_steps_before}");
 solutions = one_step_before + two_steps_before;
 two_steps_before = one_step_before;
