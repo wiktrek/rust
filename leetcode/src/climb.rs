6c15e626bf8a8f034dc2d50ce6bc65737ee71f3b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-24 20:20:32 +0100

    leetcode

--- leetcode/src/climb.rs
+++ leetcode/src/climb.rs
@@ -3,6 +3,7 @@ let mut solutions: i32 = 0;
 let mut one_step_before = 2;
 let mut two_steps_before = 1;
 for _ in 0..n {
+    println!("{solutions}, {one_step_before}, {two_steps_before}");
 solutions = one_step_before + two_steps_before;
 two_steps_before = one_step_before;
 one_step_before = solutions;
