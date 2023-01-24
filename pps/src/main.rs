c6031052a3278e741e7d33fd3960f7405f596de2
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-24 19:57:12 +0100

    no .(value example: 0.3333333333333)

--- pps/src/main.rs
+++ pps/src/main.rs
@@ -1,6 +1,8 @@
 use std::env::{args, Args};
 fn main() {
 let mut args: Args= args();
-let pops: String = args.nth(1).unwrap();
-let time:String= args.nth(0).unwrap();
+let pops: i32 = args.nth(1).unwrap().parse().unwrap();
+let time: i32 = args.nth(0).unwrap().parse().unwrap();
+let result: i32 = pops / time;
+println!("{pops} / {time} = {result}")
 }
