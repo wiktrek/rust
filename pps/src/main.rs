575142111ffe3b95378b9f2eb6c5e4b35e7235a6
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-24 19:58:51 +0100

    some changes

--- pps/src/main.rs
+++ pps/src/main.rs
@@ -4,5 +4,5 @@ let mut args: Args= args();
 let pops: f64 = args.nth(1).unwrap().parse().unwrap();
 let time: f64 = args.nth(0).unwrap().parse().unwrap();
 let result: f64 = pops / time;
-println!("{pops} / {time} = {result}")
+println!("{pops} pops / {time}s = {result} pops per second")
 }
