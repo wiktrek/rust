160fedec00629bdc99c95efa85f975d784d7e56f
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-29 00:25:52 +0100

    fn main

--- spotifyapi/src/main.rs
+++ spotifyapi/src/main.rs
@@ -1,4 +1,4 @@
-use reqwest::{self, header};
+use reqwest;
 use reqwest::header::{ACCEPT,AUTHORIZATION,CONTENT_TYPE};
 use serde::{Deserialize,Serialize};
 use std::env;
@@ -27,14 +27,14 @@ popularity: u32,
 album: Album,
 external_urls: ExternalUrls,
 }
-#[derive(Serialize, Deserialize, Debug)]
-struct APIResponse{
-    tracks: Items<Track>,
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Items<T> {
     items: Vec<T>
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct APIResponse{
+    tracks: Items<Track>,
 }
 fn print_tracks(tracks: Vec<&Track>) {
 for track in tracks{
@@ -51,31 +51,30 @@ async fn main() {
     let search_query = &args[1];
     let auth_token = &args[2];
     let url = format!(
-      "https://api.spotify.com/search?q={query}&type=track,artist",
-      query = search_query
+        "https://api.spotify.com/v1/search?q={query}&type=track,artist",
+        query = search_query
     );
     let client = reqwest::Client::new();
     let response = client
-    .get(url)
-    .header(AUTHORIZATION, format!("Bearer {}", auth_token))
-    .header(CONTENT_TYPE, "application/json")
-    .header(ACCEPT, "application/jso")
-    .send()
-    .await
-    .unwrap();
+        .get(url)
+        .header(AUTHORIZATION, format!("Bearer {}", auth_token))
+        .header(CONTENT_TYPE, "application/json")
+        .header(ACCEPT, "application/json")
+        .send()
+        .await
+        .unwrap();
     match response.status() {
         reqwest::StatusCode::OK => {
             match response.json::<APIResponse>().await {
                 Ok(parsed) => print_tracks(parsed.tracks.items.iter().collect()),
-                Err(_) => println!("the response didn't match the struct / shape")
-            }
+                Err(_) => println!("Hm, the response didn't match the shape we expected."),
+            };
         }
         reqwest::StatusCode::UNAUTHORIZED => {
             println!("Need to grab a new token");
         }
         other => {
-            panic!("something unexpected happened")
+            panic!("Uh oh! Something unexpected happened: {:?}", other);
         }
-    }
-
+    };
 }
