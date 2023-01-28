e6f8fc2f8d90cd25b9b7bb8bad42340a509c509d
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-29 00:26:28 +0100

    fn main

--- spotifyapi/src/main.rs
+++ spotifyapi/src/main.rs
@@ -1,50 +1,63 @@
 use reqwest;
-use reqwest::header::{ACCEPT,AUTHORIZATION,CONTENT_TYPE};
-use serde::{Deserialize,Serialize};
+use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use serde::{Deserialize, Serialize};
 use std::env;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct ExternalUrls{
+struct ExternalUrls {
     spotify: String,
 }
+
 #[derive(Serialize, Deserialize, Debug)]
-struct Artist{
+struct Artist {
     name: String,
-    external_url: ExternalUrls,
+    external_urls: ExternalUrls,
 }
+
 #[derive(Serialize, Deserialize, Debug)]
-struct Album{
+struct Album {
     name: String,
     artists: Vec<Artist>,
     external_urls: ExternalUrls,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Track{
-name: String,
-href: String,
-popularity: u32,
-album: Album,
-external_urls: ExternalUrls,
+struct Track {
+    name: String,
+    href: String,
+    popularity: u32,
+    album: Album,
+    external_urls: ExternalUrls,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Items<T> {
-    items: Vec<T>
+    items: Vec<T>,
 }
+
 #[derive(Serialize, Deserialize, Debug)]
-struct APIResponse{
+struct APIResponse {
     tracks: Items<Track>,
 }
+
 fn print_tracks(tracks: Vec<&Track>) {
-for track in tracks{
-    println!("{}", track.name);
-    println!("{}", track.album.name);
-    println!("{}", track.album.artists.iter().map(|artist| artist.name.to_string()).collect::<String>());
-    println!("{}", track.external_urls.spotify);
-    println!("----------------");
+    for track in tracks {
+        println!("{}", track.name);
+        println!("{}", track.album.name);
+        println!(
+            "{}",
+            track
+                .album
+                .artists
+                .iter()
+                .map(|artist| artist.name.to_string())
+                .collect::<String>()
+        );
+        println!("{}", track.external_urls.spotify);
+        println!("---------")
+    }
 }
-}
+
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
