eca7a62c2bb29e4e93c9007f55c9d92eb723e00f
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-28 23:53:24 +0100

    spotifyapi

--- spotifyapi/src/main.rs
+++ spotifyapi/src/main.rs
@@ -2,3 +2,10 @@ use reqwest;
 use reqwest::header::{ACCEPT,AUTHORIZATION,CONTENT_TYPE}
 use serde::{Deserialize,Serialize};
 use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExternalUrls{
+    spotify: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Atrist{}
