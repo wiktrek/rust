89b3dda3f012242ee260093870b92093c617a15b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-01 22:56:59 +0100

    crudapi

--- crudapi/src/main.rs
+++ crudapi/src/main.rs
@@ -7,7 +7,7 @@ use rocket::http::Status;
 use rocket::response::Redirect;
 use rocket::http::uri::Origin;
 use rocket::serde::json::{json, Value};
-use reqwest;
+use reqwest::{self, Response};
 use reqwest::Client;
 const SEARCH_PREFIX: Origin<'static> = uri!("/tauri-releases");
 const REPO: &str = "elibroftw/google-keep-desktop-app";
@@ -22,19 +22,26 @@ fn create_tauri_response(github_release: &Value) -> Option<Value> {
         ("app.tar.gz", vec!["darwin-x86_64", "darwin-aarch64"]),
         ("x64_en-US.msi.zip", vec!["windows-x86_64"])
         ]);
-let mut response = json!({
+let mut response: Value = json!({
     "version": github_release["tag_name"].as_str()?,
     "notes": remove_suffix(&github_release["body"].as_str()?, "See the assets to download this version and install.").trim_end_mathes(['\r', '\n', ' ']),
     "pub_date": github_release["published_at"].as_str()?,
     "platforms": {}
 });
+let response_platforms = release["platforms"].as_object()?;
+for asset in github_release["assets"].as_array().iter() {
+    let asset = asset.as_object()?;
+    let asset_name = asset["name"].as_str()?;
+    for (extension, os_arch) in platforms_available.iter() {
 
+    }
+}
 Some(response)
 }
 async fn get_latest_release(client: &Client,repo: &str)  -> Result<Value,reqwest::Error> {
-    let url = format!("http://api.github.com/repos/{repo}/releases/latest");
-    let response = client.get(&url).send().await?;
-    let github_release = response.json::<Value>().await?;
+    let url: String = format!("http://api.github.com/repos/{repo}/releases/latest");
+    let response: Response = client.get(&url).send().await?;
+    let github_release: Value = response.json::<Value>().await?;
     create_tauri_response(&github_release).or(json!({})).or_else(|e| Ok(e));
     Ok(github_release)
 }
