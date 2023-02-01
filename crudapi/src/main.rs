f60b05e31cb91e0b4dcc556e8b04c0415ce1943b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-01 23:08:41 +0100

    crudapi

--- crudapi/src/main.rs
+++ crudapi/src/main.rs
@@ -1,6 +1,5 @@
 #[macro_use]
 extern crate rocket;
-use std::collections::HashMap;
 
 use rocket::State;
 use rocket::http::Status;
@@ -9,6 +8,7 @@ use rocket::http::uri::Origin;
 use rocket::serde::json::{json, Value};
 use reqwest::{self, Response};
 use reqwest::Client;
+use std::collections::HashMap;
 const SEARCH_PREFIX: Origin<'static> = uri!("/tauri-releases");
 const REPO: &str = "elibroftw/google-keep-desktop-app";
 #[get("/")]
@@ -16,33 +16,64 @@ fn index() -> Redirect {
     let msg:Option<&str> = None;
     Redirect::to(uri!(SEARCH_PREFIX, search(msg)))
 }
-fn create_tauri_response(github_release: &Value) -> Option<Value> {
-    let platforms_available: HashMap<&str, Vec<&str>> = HashMap::from([
-        ("amd64.AppImage.tar.gz", vec!["linux-x84_64"]),
-        ("app.tar.gz", vec!["darwin-x86_64", "darwin-aarch64"]),
-        ("x64_en-US.msi.zip", vec!["windows-x86_64"])
-        ]);
-let mut response: Value = json!({
-    "version": github_release["tag_name"].as_str()?,
-    "notes": remove_suffix(&github_release["body"].as_str()?, "See the assets to download this version and install.").trim_end_mathes(['\r', '\n', ' ']),
-    "pub_date": github_release["published_at"].as_str()?,
-    "platforms": {}
-});
-let response_platforms = release["platforms"].as_object()?;
-for asset in github_release["assets"].as_array().iter() {
-    let asset = asset.as_object()?;
-    let asset_name = asset["name"].as_str()?;
-    for (extension, os_arch) in platforms_available.iter() {
-
+fn remove_suffix<'a>(s: &'a str, suffix: &str) -> &'a str {
+    match s.strip_suffix(suffix) {
+        Some(s) => s,
+        None => s,
     }
 }
-Some(response)
+
+async fn text_request(client: &State<Client>, url: &str) -> Result<String, reqwest::Error> {
+    client.get(url).send().await?.text().await
+}
+async fn create_tauri_response(client: &State<Client>, github_release: &Value) -> Option<Value> {
+    let platforms_available: HashMap<&str, Vec<&str>> = HashMap::from([
+        ("amd64.AppImage.tar.gz", vec!["linux-x86_64"]),
+        ("app.tar.gz", vec!["darwin-x86_64", "darwin-aarch64"]),
+        ("x64_en-US.msi.zip", vec!["windows-x86_64"]),
+    ]);
+
+    let mut response = json!({
+        "version": github_release["tag_name"].as_str()?,
+        "notes": remove_suffix(&github_release["body"].as_str()?, "See the assets to download this version and install.").trim_end_matches(['\r', '\n', ' ']),
+        "pub_date": github_release["published_at"].as_str()?,
+        "platforms": {}
+    });
+
+    let response_platforms = response["platforms"].as_object_mut()?;
+    for asset in github_release["assets"].as_array()?.iter() {
+        let asset = asset.as_object()?;
+        let asset_name = asset["name"].as_str()?;
+        let browser_download_url = asset["browser_download_url"].as_str()?;
+        for (extension, os_archs) in platforms_available.iter() {
+            if asset_name.ends_with(extension) {
+                for os_arch in os_archs.iter() {
+                    if !response_platforms.contains_key(*os_arch) {
+                        response_platforms.insert(os_arch.to_string(), json!({}));
+                    }
+                    response_platforms[*os_arch].as_object_mut()?.insert("url".to_string(), Value::String(browser_download_url.to_string()));
+                }
+            } else if asset_name.ends_with(&format!("{extension}.sig")) {
+                let signature = match text_request(client, browser_download_url).await {
+                    Ok(s) => s,
+                    _ => String::new()
+                };
+                for os_arch in os_archs.iter() {
+                    if !response_platforms.contains_key(*os_arch) {
+                        response_platforms.insert(os_arch.to_string(), json!({}));
+                    }
+                    response_platforms[*os_arch].as_object_mut()?.insert("signature".to_string(), Value::String(signature.clone()));
+                }
+            }
+        }
+    }
+    Some(response)
 }
 async fn get_latest_release(client: &Client,repo: &str)  -> Result<Value,reqwest::Error> {
     let url: String = format!("http://api.github.com/repos/{repo}/releases/latest");
     let response: Response = client.get(&url).send().await?;
     let github_release: Value = response.json::<Value>().await?;
-    create_tauri_response(&github_release).or(json!({})).or_else(|e| Ok(e));
+    create_tauri_response(client, &github_release).await.ok_or(json!({})).or_else(|e| Ok(e));
     Ok(github_release)
 }
 #[get("/search?<msg>")]
