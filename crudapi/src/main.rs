51290e25fdd189d582edba3ae50755674d903642
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-01 22:51:18 +0100

    hashmap

--- crudapi/src/main.rs
+++ crudapi/src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate rocket;
+use std::collections::HashMap;
+
 use rocket::State;
 use rocket::http::Status;
 use rocket::response::Redirect;
@@ -7,27 +9,33 @@ use rocket::http::uri::Origin;
 use rocket::serde::json::{json, Value};
 use reqwest;
 use reqwest::Client;
-const SEARCH_PREFIX: Origin<'static> = uri!("/search");
-const REPO: &str = "wiktrek/rust";
+const SEARCH_PREFIX: Origin<'static> = uri!("/tauri-releases");
+const REPO: &str = "elibroftw/google-keep-desktop-app";
 #[get("/")]
 fn index() -> Redirect {
     let msg:Option<&str> = None;
     Redirect::to(uri!(SEARCH_PREFIX, search(msg)))
 }
-fn make_tauri_response(github_release: &Value) -> Option<Value> {
+fn create_tauri_response(github_release: &Value) -> Option<Value> {
+    let platforms_available: HashMap<&str, Vec<&str>> = HashMap::from([
+        ("amd64.AppImage.tar.gz", vec!["linux-x84_64"]),
+        ("app.tar.gz", vec!["darwin-x86_64", "darwin-aarch64"]),
+        ("x64_en-US.msi.zip", vec!["windows-x86_64"])
+        ]);
 let mut response = json!({
-    "version": github_release["tag_name"].as_str().ok_or("tag_name not found")?,
+    "version": github_release["tag_name"].as_str()?,
     "notes": remove_suffix(&github_release["body"].as_str()?, "See the assets to download this version and install.").trim_end_mathes(['\r', '\n', ' ']),
     "pub_date": github_release["published_at"].as_str()?,
-    
+    "platforms": {}
 });
 
+Some(response)
 }
 async fn get_latest_release(client: &Client,repo: &str)  -> Result<Value,reqwest::Error> {
     let url = format!("http://api.github.com/repos/{repo}/releases/latest");
     let response = client.get(&url).send().await?;
     let github_release = response.json::<Value>().await?;
-    make_tauri_response(&github_release).or(json!({})).or_else(|e| Ok(e));
+    create_tauri_response(&github_release).or(json!({})).or_else(|e| Ok(e));
     Ok(github_release)
 }
 #[get("/search?<msg>")]
