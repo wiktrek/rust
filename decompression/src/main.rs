0bfb039ef816a08de24a91c157849014a85b8dee
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-09 23:35:08 +0100

    decompression

--- decompression/src/main.rs
+++ decompression/src/main.rs
@@ -42,7 +42,36 @@ fn real_main() -> i32 {
             }
         }
 //the zip can contain other folders too
+if (*file.name()).ends_with('/') {
+    println!("File {} extracted to \"{}\"", i, outpath.display());
+//recursively create a new directory
+    fs::create_dir_all(&outpath).unwrap();
+} else {
+    println!(
+        "File {} extracted to \"{}\" ({} bytes)",
+        i,
+        outpath.display(),
+        file.size()
+    );
+    //if there is no parent for those files, create a new directory
+    if let Some(p) = outpath.parent() {
+        if !p.exists() {
+            fs::create_dir_all(&p).unwrap();
+        }
+    }
+    let mut outfile = fs::File::create(&outpath).unwrap();
+    io::copy(&mut file, &mut outfile).unwrap();
+}
 
+// Get and Set permissions for the extracted files
+#[cfg(unix)]
+{
+    use std::os::unix::fs::PermissionsExt;
+
+    if let Some(mode) = file.unix_mode() {
+        fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+    }
+}
     }
 
     0
