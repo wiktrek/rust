e6b72f06721615eb14667b3f5a0f81923f4ac0b3
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-10 23:28:45 +0100

    writejson

--- writejson/src/main.rs
+++ writejson/src/main.rs
@@ -1,3 +1,16 @@
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize)]
+struct Paragraph {
+    name: String
+}
+
+
+#[derive(Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>
+}
 fn main() {
     println!("Hello, world!");
 }
