f039b2b6056974f51eaeff7c4ff4c9b48f0dd308
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-10 23:26:48 +0100

    readcsv

--- readcsv/src/main.rs
+++ readcsv/src/main.rs
@@ -1,17 +1,19 @@
 use std::error::Error;
+
 use csv;
 
-fn main() {
-    if let Err(e) = read_from_file("./e.csv") {
-        eprintln!("{}", e)
-    }
-}
-fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>>{
     let mut reader = csv::Reader::from_path(path)?;
-    for result in reader.records() {
+
+    for result in reader.records(){
         let record = result?;
         println!("{:?}", record);
-
     }
     Ok(())
+}
+
+fn main() {
+    if let Err(e) = read_from_file("./e.csv"){
+        eprintln!("{}", e);
+    }
 }
