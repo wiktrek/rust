4a5b5d58a9fde0c61846eb4e3c53120d9cf3882a
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-12 20:38:40 +0100

    percentage

--- percentage/src/main.rs
+++ percentage/src/main.rs
@@ -1,4 +1,16 @@
 use std::env;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: <number> <number>");
+        println!("Example: 1 2 output: 50%");
+        return;
+    }
+    let a: f64 = args[1].parse::<f64>().unwrap();
+    let b: f64 = args[2].parse::<f64>().unwrap();
+    let percentage: f64 = a / b * 100 as f64;
+    println!("{:?}%", percentage);
+}
 
 
 
