41ddd4bcb5673872005c501f7e506ee357998bda
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-12 20:55:07 +0100

    removed cmd

--- cmd/src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::process::Command;
-fn main() {
-    let output = "none";
-
-    let mut list_dir = Command::new(r"cd target");
-    list_dir.status().expect("process failed to execute");
-    println!("{:?}", output);
-}
