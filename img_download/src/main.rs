4c32a645017c7c254456ddef9f319c4086b8c921
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-14 22:12:23 +0100

    more errors img_download

--- img_download/src/main.rs
+++ img_download/src/main.rs
@@ -15,5 +15,17 @@ fn main() -> Result<()> {
 let tmp_dir = Builder::new().prefix("example").tempdir();
 let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
 let response = reqwest::get(target).await?;
+
+let mut dest = {
+    let fname = response
+    .url()
+    .path_segments()
+    .and_then(|segments| segments.last())
+    .and_then(|name| if name.is_empty() {None} else {Some(name)})
+    .unwrap();
+println!("file to download: {}", fname);
+let fname = tmp_dir.path().join(fname);
+println!("will be located under '{:?}'", fname);
+}
 Ok(())
 }
