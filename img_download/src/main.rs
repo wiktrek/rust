0fc614cc17e86041db13290197e0bdead2f02977
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-14 22:07:48 +0100

    img_download

--- img_download/src/main.rs
+++ img_download/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+use std::io::copy;
+use std::fs::File;
+use tempfile::Builder;
+
+error_chain!{
+    foreign_links{
+        Io(std::io::Error);
+        HttRequest(reqwest::Error);
+    }          
+}
+
+#[tokio::main]
+fn main() -> Result<()> {
+let tmp_dir = Builder::new().prefix("example").tempdir();
+let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+let response = reqwest::get(target).await?;
+Ok(())
 }
