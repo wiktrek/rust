ceb36741080c2feadfe1047636ee4cdcfeec63ce
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-14 22:19:13 +0100

    img_download

--- img_download/src/main.rs
+++ img_download/src/main.rs
@@ -3,32 +3,33 @@ use std::io::copy;
 use std::fs::File;
 use tempfile::Builder;
 
-error_chain!{
-    foreign_links{
-        Io(std::io::Error);
-        HttRequest(reqwest::Error);
-    }          
+error_chain! {
+     foreign_links {
+         Io(std::io::Error);
+         HttpRequest(reqwest::Error);
+     }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-let tmp_dir = Builder::new().prefix("example").tempdir()?;
-let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
-let response = reqwest::get(target).await?;
+    let tmp_dir = Builder::new().prefix("example").tempdir()?;
+    let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+    let response = reqwest::get(target).await?;
 
-let mut dest = {
-    let fname = response
-    .url()
-    .path_segments()
-    .and_then(|segments| segments.last())
-    .and_then(|name| if name.is_empty() {None} else {Some(name)})
-    .unwrap();
-println!("file to download: {}", fname);
-let fname = tmp_dir.path().join(fname);
-println!("will be located under '{:?}'", fname);
-File::create(fname)?
-};
-let content = response.text().await?;
-copy(&mut content.as_bytes(), &mut dest)?;
-Ok(())
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+        println!("file to download: '{}'", fname);
+        let fname = tmp_dir.path().join(fname);
+        println!("will be located under: '{:?}'", fname);
+        File::create(fname)?
+    };
+    let content =  response.text().await?;
+    copy(&mut content.as_bytes(), &mut dest)?;
+    Ok(())
 }
