c5a3f4c644b25de5ac1a53620e031119bd7a7741
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-19 20:56:28 +0100

    works

--- testapi/src/routes/test.rs
+++ testapi/src/routes/test.rs
@@ -1,5 +1,21 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 use rocket::*;
+
 #[get("/save/<user>/<data>")]
 pub fn test(user: String, data: String) -> String{
+let path = Path::new("src/files/").with_file_name(user.clone());
+let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create: {}", why),
+        Ok(file) => file,
+    };
+
+    match file.write_all(data.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    };
     format!("data:{data}, user: {user}")
 }
