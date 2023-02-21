e4f6632bde218a0e50431125b3c4229eb7370562
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-21 21:48:51 +0100

    floor gen

--- games/game/src/main.rs
+++ games/game/src/main.rs
@@ -1,10 +1,29 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-    let mut user_input = String::new();
-    let stdin = io::stdin();
-    stdin.read_line(&mut user_input).expect("Couldn’t read from stdin");
+    let mut floor: Vec<String> = floor_gen();
 
-    println!("input {} ", user_input);
+    
+    floor.push(read_line());
+    println!("floor {:?}", floor);
+  
     Ok(())
+}
+fn read_line() -> String{
+    let mut user_input = String::new();
+    println!("Enter a number");
+    io::stdin()
+    .read_line(&mut user_input).expect("Couldn’t read from stdin");
+
+    return user_input.clone().replace("\r\n", "")
+}
+fn floor_gen() -> Vec<String>{
+    // floor: "0"..."9"
+    let mut floor: Vec<String> = vec![];
+    let mut a = 0;
+    while a < 11 {
+        floor.push(a.to_string());
+        a += 1;
+    }
+    return floor
 }
