c5f46917289ef99b79583e37c0af0e2da54124ee
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-21 21:21:08 +0100

    read_line

--- games/game/src/main.rs
+++ games/game/src/main.rs
@@ -1,4 +1,10 @@
 use std::io;
-fn main() {
-    println!("Hello, world!");
+
+fn main() -> io::Result<()> {
+    let mut user_input = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut user_input).expect("Couldn’t read from stdin");
+
+    println!("input {} ", user_input);
+    Ok(())
 }
