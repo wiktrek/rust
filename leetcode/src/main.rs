e3031e5982c98d5649efdb26ea363a138971cd30
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-23 21:36:04 +0100

    Plus one

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,6 +1,13 @@
+
 fn main() {
     let digits: Vec<i32> = vec![1, 2, 3];
 
-    println!("{}", digits.last());
+    println!("{:?}", digits.last());
+    let mut anwser: Vec<i32> = vec![];
+    let mut split: i32 = format!("{:?}", digits).replace("[", "").replace("]", "").replace(", ", "").parse().unwrap();
+    split += 1;
+    let char_vec: Vec<char> = split.to_string().chars().collect();
+
+    println!("{:?}", anwser);
     return;
 }
