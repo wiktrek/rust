51bf16d8d186b36791e98c79288f1c62df09e02c
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-25 23:47:33 +0100

    days_ago not working

--- Math/days_ago/src/main.rs
+++ Math/days_ago/src/main.rs
@@ -1,13 +1,13 @@
 use std::io;
-use chrono::{DateTime, FixedOffset, Local, Utc};
+use chrono::prelude::*;
 fn main() {
     let local_time = Local::now().timestamp();
     let mut user_input = String::new();
     println!("enter date");
     io::stdin()
     .read_line(&mut user_input).expect("Couldn’t read from stdin");
-let b = DateTime::timestamp();
-let a= local_time - b.
+let b = Utc.with_ymd_and_hms(2017, 04, 02, 12, 50, 32).unwrap().timestamp();
+let a= local_time - b;
 
 println!("{}, {}", a, local_time);
 }
