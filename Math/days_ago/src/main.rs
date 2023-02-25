47e14504179de4d55c7e9aaf6bd35d900f8b7f45
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-25 23:52:08 +0100

    days_ago not working

--- Math/days_ago/src/main.rs
+++ Math/days_ago/src/main.rs
@@ -2,12 +2,20 @@ use std::io;
 use chrono::prelude::*;
 fn main() {
     let local_time = Local::now().timestamp();
-    let mut user_input = String::new();
-    println!("enter date");
+    let mut day = String::new();
+    println!("enter the day");
     io::stdin()
-    .read_line(&mut user_input).expect("Couldn’t read from stdin");
-let b = Utc.with_ymd_and_hms(2017, 04, 02, 12, 50, 32).unwrap().timestamp();
+    .read_line(&mut day).expect("Couldn’t read from stdin");
+let mut month = String::new();
+println!("enter the month");
+io::stdin()
+.read_line(&mut month).expect("Couldn’t read from stdin");
+let mut year = String::new();
+println!("enter the year");
+io::stdin()
+.read_line(&mut year).expect("Couldn’t read from stdin");
+let b = Utc.with_ymd_and_hms(year.parse().unwrap(), month.parse().unwrap(), day.parse().unwrap(), 12, 50, 0).unwrap().timestamp();
 let a= local_time - b;
-
-println!("{}, {}", a, local_time);
+let c = a / 60 / 60 / 24;
+println!("{}, {}", c, local_time);
 }
