a6f2f8218d8664a16faf1252fa5a381f7a9d5223
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-26 22:40:53 +0100

    .

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,10 +1,4 @@
 fn main() {
-    let a = "101";
-    let b = "1";
-    let n: String = "egnikasd".to_string();
-    // println!("{}", format!("{:b}",a.parse::<i32>().unwrap()))
-    // println!("{:?}", i32::from_str_radix(a, 2).unwrap())
-    let c =  i32::from_str_radix(b, 2).unwrap() + i32::from_str_radix(a, 2).unwrap();
-    println!("{}", format!("{:b}", c));
+
 
 }
