2768897f31550c4a01c443d520069804d30e279b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-26 22:40:38 +0100

    Add Binary

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    let a = "0101";
+    let a = "101";
     let b = "1";
-    format!("{:b}");
+    let n: String = "egnikasd".to_string();
+    // println!("{}", format!("{:b}",a.parse::<i32>().unwrap()))
+    // println!("{:?}", i32::from_str_radix(a, 2).unwrap())
+    let c =  i32::from_str_radix(b, 2).unwrap() + i32::from_str_radix(a, 2).unwrap();
+    println!("{}", format!("{:b}", c));
+
 }
