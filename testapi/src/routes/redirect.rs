c4100bc92dcff5c8fd874dcfa2f4b9f0f8502bdf
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-04 20:12:32 +0100

    redirect.rs

--- testapi/src/routes/redirect.rs
+++ testapi/src/routes/redirect.rs
@@ -3,5 +3,6 @@ use rocket::response::Redirect;
 #[get("/redirect/<link>")]
 pub fn redirect(link: &str) -> Redirect {
     let go = format!("https://wiktrek.xyz/go/{}", link);
+    println!("{}, {}", go, link);
     Redirect::to(go)
 }
