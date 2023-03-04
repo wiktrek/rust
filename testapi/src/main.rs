fa4975d2d366c03c49b0933f5d2e67633c333f3f
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-04 19:28:52 +0100

    redirect

--- testapi/src/main.rs
+++ testapi/src/main.rs
@@ -1,8 +1,12 @@
 use rocket::*;
 mod routes;
-
-
+use routes::date::date;
+use routes::hello::hello;
+use routes::index::index;
+use routes::file::save;
+use routes::file::delete;
+use routes::redirect::redirect;
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![routes::hello::hello, routes::index::index, routes::file::save, routes::file::delete, routes::date::date, routes::redirect::redirect])
+    rocket::build().mount("/", routes![hello, index, save, delete, date, redirect])
 }
