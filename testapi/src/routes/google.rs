df2debfc7a0185382207b614aaafee4fb6f31c94
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-05 21:29:24 +0100

    google.rs

--- testapi/src/routes/google.rs
+++ testapi/src/routes/google.rs
@@ -1,6 +1,6 @@
 use rocket::*;
 use rocket::response::Redirect;
-#[get("/google?<query>")]
+#[get("/google/<query>")]
 pub fn google(query: &str) -> Redirect {
     let go = format!("https://www.google.com/search?q={}", query);
     println!("{}, {}", go, query);
