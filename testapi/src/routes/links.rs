0baacdb5e624b80af13099fcd39a4e291abed4c3
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-05 21:46:50 +0100

    links.rs

--- testapi/src/routes/links.rs
+++ testapi/src/routes/links.rs
@@ -5,3 +5,18 @@ pub fn yt() -> Redirect{
     let go = "https://wiktrek.xyz/yt";
     Redirect::to(go)
 }
+#[get("/ig")]
+pub fn ig() -> Redirect{
+    let go = "https://wiktrek.xyz/ig";
+    Redirect::to(go)
+}
+#[get("/gh")]
+pub fn gh() -> Redirect{
+    let go = "https://wiktrek.xyz/gh";
+    Redirect::to(go)
+}
+#[get("/ttv")]
+pub fn ttv() -> Redirect{
+    let go = "https://wiktrek.xyz/ttv";
+    Redirect::to(go)
+}
