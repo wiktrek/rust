08e09c436a30fc68dfc7e5ba9de4af1ee3567249
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-05 21:29:36 +0100

    google.rs

--- testapi/src/routes/google.rs
+++
@@ -1,8 +0,0 @@
-use rocket::*;
-use rocket::response::Redirect;
-#[get("/google/<query>")]
-pub fn google(query: &str) -> Redirect {
-    let go = format!("https://www.google.com/search?q={}", query);
-    println!("{}, {}", go, query);
-    Redirect::to(go)
-}
--- testapi/src/routes/mod.rs
+++ testapi/src/routes/mod.rs
@@ -2,10 +2,9 @@ mod index;
 mod file;
 mod date;
 mod redirect;
-mod google;
+
 pub use index::index as index_rs;
 pub use file::save;
 pub use file::delete;
 pub use date::date as date_rs;
 pub use redirect::redirect as redirect_rs;
-pub use google::google as google_rs;
