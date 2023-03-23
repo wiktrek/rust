79b80cb73c3b42764a82bfc7210085d3e4574e8a
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-23 23:16:44 +0100

    .

--- file/fs/src/main.rs
+++ file/fs/src/main.rs
@@ -1,10 +1,15 @@
 use std::{ io, fs};
 fn main() {
     let mut action: String = String::new(); 
+    
     println!("Hello, world!");
 io::stdin()
     .read_line(&mut action).expect("Couldn’t read from stdin");
-
-    match action {
+    let a: &str = action.parse().unwrap();
+    match a {
+    "ez" => rename()
     }
+}
+fn rename() {
+
 }
