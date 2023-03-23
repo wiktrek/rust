5512634e94d03388d59c98b328db831635dde6aa
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-23 23:27:35 +0100

    .

--- file/fs/src/main.rs
+++ file/fs/src/main.rs
@@ -15,7 +15,27 @@ io::stdin()
     }
 }
 fn rename() {
-println!("rename")
+    let mut path: String = String::new(); 
+    let mut path_to: String = String::new(); 
+
+    println!("path to the directory you want to rename");
+
+io::stdin()
+    .read_line(&mut path).expect("Couldn’t read from stdin");
+io::stdin()
+    .read_line(&mut path_to).expect("Couldn’t read from stdin");
+
+    println!("path to the directory you want to rename to"); 
+    let replace_path = path.replace("\r\n", "").replace(" ", "");
+    let replace_path_to = path_to.replace("\r\n", "").replace(" ", "");
+
+
+
+
+println!("rename");
+    let rename = fs::rename("", "");
+    println!("{:?}", rename);
+    
 }
 fn create() {
     println!("create")
