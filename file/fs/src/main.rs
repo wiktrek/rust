414b87c6d26f98978a555ae058a37cf81708de9a
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-23 23:12:55 +0100

    .

--- file/fs/src/main.rs
+++ file/fs/src/main.rs
@@ -1,3 +1,10 @@
+use std::{ io, fs};
 fn main() {
+    let mut action: String = String::new(); 
     println!("Hello, world!");
+io::stdin()
+    .read_line(&mut action).expect("Couldn’t read from stdin");
+
+    match action {
+    }
 }
