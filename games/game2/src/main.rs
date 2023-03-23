4af9688e8e840134f2630e5f0af223ad61514363
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-23 23:06:19 +0100

    game2

--- games/game2/src/main.rs
+++ games/game2/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut user_input = String::new();
+    println!("Enter the radius of a circle in cm");
+    io::stdin()
+    .read_line(&mut user_input).expect("Couldn’t read from stdin");
+let pi: f64 = 3.14159265359;
+let a: f64 = user_input.replace("\r\n", "").parse().unwrap();
+println!("{}cm²", (pi*(a*a)));
 }
+
