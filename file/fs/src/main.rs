52b0adacd0ce3a7d238e5c76ceddcd5938c841ce
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-24 22:09:40 +0100

    remove

--- file/fs/src/main.rs
+++ file/fs/src/main.rs
@@ -68,5 +68,20 @@ io::stdin()
 }
 fn remove() ->  Result<(), std::io::Error> {
     println!("remove");
-    Ok(())
+
+    let mut path: String = String::new(); 
+
+    println!("path to the directory you want to create");
+
+io::stdin()
+    .read_line(&mut path).expect("Couldn’t read from stdin");
+    println!("path to the directory you want to rename to"); 
+
+
+    let replace_path = path.replace("\r\n", "").replace(" ", "");
+
+    let result = fs::remove_dir(replace_path);
+
+
+    result
 }
