7acb853446c05492591ceaba856a0e1bdac4c3a1
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-24 22:09:05 +0100

    create

--- file/fs/src/main.rs
+++ file/fs/src/main.rs
@@ -1,8 +1,11 @@
 use std::{ io, fs};
-fn main() -> Result<(), std::io::Error> {
-    let mut action: String = String::new(); 
-    
+fn main() -> Result<(), std::io::Error>{
     println!("Hello, world!");
+start()
+}
+fn start() -> Result<(), std::io::Error>{
+    let mut action: String = String::new(); 
+
 io::stdin()
     .read_line(&mut action).expect("Couldn’t read from stdin");
     let replace = action.replace("\r\n", "").replace(" ", "");
@@ -11,6 +14,7 @@ io::stdin()
     "create" => create(),
     "remove" => remove(),
     "rename" => return rename(),
+    "options" => options(),
     _ => panic!("error, picked: {}", a)
     }
 }
@@ -22,10 +26,11 @@ fn rename() -> Result<(), std::io::Error> {
 
 io::stdin()
     .read_line(&mut path).expect("Couldn’t read from stdin");
+    println!("path to the directory you want to rename to"); 
 io::stdin()
     .read_line(&mut path_to).expect("Couldn’t read from stdin");
 
-    println!("path to the directory you want to rename to"); 
+
     let replace_path = path.replace("\r\n", "").replace(" ", "");
     let replace_path_to = path_to.replace("\r\n", "").replace(" ", "");
 
@@ -35,9 +40,31 @@ io::stdin()
 println!("rename");
     result
 }
+fn options() -> Result<(), std::io::Error> {
+    println!("rename");
+    print!("create");
+    println!("remove");
+
+    Ok(())
+}
 fn create() ->  Result<(), std::io::Error>{
     println!("create");
-    Ok(())
+
+    let mut path: String = String::new(); 
+
+    println!("path to the directory you want to create");
+
+io::stdin()
+    .read_line(&mut path).expect("Couldn’t read from stdin");
+    println!("path to the directory you want to rename to"); 
+
+
+    let replace_path = path.replace("\r\n", "").replace(" ", "");
+
+    let result = fs::create_dir(replace_path);
+
+
+    result
 }
 fn remove() ->  Result<(), std::io::Error> {
     println!("remove");
