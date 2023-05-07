065d338bd26f47746be4432fa7f9b303e2de568e
wiktrek <wiktor.janiszewski10@gmail.com>
2023-05-07 17:05:32 +0200

    leetcode

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
-    let n: i32;
+    let mut n1: i32 = 0;
+    let mut n2: i32 = 0;
     let l1 = [1, 2, 3];
+    let l2 = [4, 5, 6];
     for (index, i) in l1.iter().enumerate() {
-        println!("{}, {}", i, index)
+        println!("{}, {}", i, index);
+        n1 = n1 + i * i32::pow(10, index as u32);
     }
+    println!("{}", n1);
     let output = [1];
 }
