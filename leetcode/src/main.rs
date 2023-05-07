fe76bb577d15f43acd7efacf1fa068a85e5883fb
wiktrek <wiktor.janiszewski10@gmail.com>
2023-05-07 17:39:13 +0200

    leetcode

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+fn main() {
+    // leetcode 746. Min Cost Climbing Stairs
+    let cost = [10, 15, 20];
+    let mut prev_cost = 0;
+    let steps_needed = cost.len() + 1;
+
+    for (index, i) in cost.iter().enumerate() {
+        
+    }
+}
