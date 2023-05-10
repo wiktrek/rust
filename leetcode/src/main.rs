7c00174d2c9c301e45f9c4711c67f80f1c5b05ed
wiktrek <wiktor.janiszewski10@gmail.com>
2023-05-10 23:08:32 +0200

    .

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,17 +1,1 @@
-fn main() {
-    // leetcode 746. Min Cost Climbing Stairs
-    let cost = [1, 100, 1, 100, 400, 1];
-    let len = cost.len();
-
-    let mut n1 = cost[0];
-    let mut n2 = cost[1];
-    // loops from 2 to len ( 5)
-    for stair in 2..len {
-        let n3 = cost[stair] + n1.min(n2);
-        // updating new states
-        n1 = n2;
-        n2 = n3
-    }
-    // result = min cost of reaching n-1 step and n-2
-    println!("{}", n1.min(n2))
-}
+fn main() {}
