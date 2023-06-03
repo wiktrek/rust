5c1022236c9a954b5627b709a948c629eba0a3ec
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-03 23:49:05 +0200

    not finished

--- algorithms/binary_search/src/main.rs
+++ algorithms/binary_search/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+fn main() {
+    let a = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let mut lo = 0;
+    let mut a_len = a.len();
+    let searching = 4;
+
+    while lo <= a_len {
+        let m = (lo + (a_len - lo) / 2);
+        let v = a[m];
+        match v {
+            _searching => return println!("Found {}", m),
+            _ => a_len = m,
+        }
+    }
+
+    println!("Not found")
+}
