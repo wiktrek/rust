ae5df22cff716adde5aaf7763fea339a7fbe3a25
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-04 00:07:01 +0200

    not finished

--- algorithms/binary_search/src/main.rs
+++ algorithms/binary_search/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 fn main() {
     let a = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let mut lo = 0;
@@ -7,10 +8,23 @@ fn main() {
     while lo <= a_len {
         let m = (lo + (a_len - lo) / 2);
         let v = a[m];
-        match v {
-            _searching => return println!("Found {}", m),
-            _ => a_len = m,
+
+        // match v {
+        //     searching => return println!("Found {m}"),
+        //     Some(v > searching) => )
+        //     _ => lo = m + 1,
+        // }
+        match v.cmp(&searching) {
+            Ordering::Greater => a_len = m,
+            Ordering::Equal => print!("Found {m} {v}"),
         }
+        // if v == searching {
+        //     return println!("Found {m} {v}");
+        // } else if v > searching {
+        //     a_len = m;
+        // } else {
+        //     lo = m + 1;
+        // }
     }
 
     println!("Not found")
