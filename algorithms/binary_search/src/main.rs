d7880e027f9491feef894cff8b4dfd563a00ba92
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-04 00:08:20 +0200

    finished ✅

--- algorithms/binary_search/src/main.rs
+++ algorithms/binary_search/src/main.rs
@@ -1,22 +1,18 @@
 use std::cmp::Ordering;
 fn main() {
+    // the array needs to be sorted
     let a = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let mut lo = 0;
     let mut a_len = a.len();
     let searching = 4;
 
     while lo <= a_len {
-        let m = (lo + (a_len - lo) / 2);
+        let m = lo + (a_len - lo) / 2;
         let v = a[m];
-
-        // match v {
-        //     searching => return println!("Found {m}"),
-        //     Some(v > searching) => )
-        //     _ => lo = m + 1,
-        // }
         match v.cmp(&searching) {
             Ordering::Greater => a_len = m,
-            Ordering::Equal => print!("Found {m} {v}"),
+            Ordering::Equal => return println!("Found {m} {v}"),
+            Ordering::Less => lo = m + 1,
         }
         // if v == searching {
         //     return println!("Found {m} {v}");
