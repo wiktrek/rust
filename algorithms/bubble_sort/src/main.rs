53ea983a10b56994717505fe0f64291d82fb4d84
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-05 22:46:17 +0200

    bubble sort

--- algorithms/bubble_sort/src/main.rs
+++ algorithms/bubble_sort/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut a = vec![34, 42, 42, 12, 521, 1, 4, 52, 21, 41234, 521];
+    let mut i = 0;
+    let n = a.len();
+    while i < n - 1 {
+        let mut j = n - 1;
+        while j > 0 {
+            if a[j] < a[j - 1] {
+                a.swap(j, j - 1)
+            }
+            j -= 1;
+        }
+        i += 1;
+    }
+    println!("{:?}", a);
 }
