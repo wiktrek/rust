a647873572fdf374a3faf086c4b1d2fb6a724a1e
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-11 23:02:31 +0200

    maze_solver

---
+++ algorithms/maze_solver/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world!");
+}
+fn solve(maze: Vec<String>, wall: String, start: Point, end: Point) -> Vec<Point> {}
--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,14 +1,3 @@
 fn main() {
-    // leetcode 49
-    use std::collections::HashMap;
-    let strs = ["eat".to_string(), "tea".to_string()];
-    let mut h = HashMap::new();
-    for s in strs {
-        let mut key: Vec<char> = s.chars().collect();
-        key.sort();
-        h.entry(key).or_insert(vec![]).push(s);
-        println!("{:?}", h)
-    }
-    let result: Vec<Vec<String>> = h.values().cloned().collect();
-    println!("{:?}", result)
+    // leetcode 347
 }
