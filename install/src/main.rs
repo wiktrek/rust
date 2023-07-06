68f9ddd8d2e5d790353a69b118445317fda9543a
wiktrek <wiktor.janiszewski10@gmail.com>
2023-07-07 00:18:07 +0200

    install output works

--- install/src/main.rs
+++ install/src/main.rs
@@ -2,7 +2,7 @@ use dialoguer::{theme::ColorfulTheme, FuzzySelect, Input};
 use std::path::Path;
 use std::process::{Command, Output};
 fn main() {
-    let options = vec!["Pnpm", "Yarn", "npm", "Other"];
+    let options = vec!["pnpm", "yarn", "npm", "Other"];
     let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Select a package manager")
         .default(0)
@@ -22,28 +22,13 @@ fn main() {
         .unwrap();
     let dir = Path::new(&input);
     let mut cmd = Command::new("cmd");
-    if selection == 0 {
-        let a: Output = cmd
-            .current_dir(dir)
-            .args(["/C", format!("pnpm i {}", package).as_str()])
-            .output()
-            .expect("process failed to execute");
-        println!("{:?}", a.stdout);
-    }
-    if selection == 1 {
-        let a: Output = cmd
-            .current_dir(dir)
-            .args(["/C", format!("yarn add {}", package).as_str()])
-            .output()
-            .expect("process failed to execute");
-        println!("{:?}", a.stdout);
-    }
-    if selection == 2 {
-        let a: Output = cmd
-            .current_dir(dir)
-            .args(["/C", format!("npm i {}", package).as_str()])
-            .output()
-            .expect("process failed to execute");
-        println!("{:?}", a.stdout);
-    }
+    let a: Output = cmd
+        .current_dir(dir)
+        .args([
+            "/C",
+            format!("{} add {}", options[selection], package).as_str(),
+        ])
+        .output()
+        .expect("process failed to execute");
+    println!("{}", std::str::from_utf8(&a.stdout).unwrap());
 }
