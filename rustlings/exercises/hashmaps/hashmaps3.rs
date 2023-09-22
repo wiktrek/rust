adbc196711c8be15b4c16db4568fe2859fa4b9aa
wiktrek <wiktor.janiszewski10@gmail.com>
2023-09-22 19:47:29 +0200

    hashmaps quiz2

--- rustlings/exercises/hashmaps/hashmaps3.rs
+++ rustlings/exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -34,6 +32,34 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+
+        if scores.contains_key(&team_1_name) {
+            let value = scores.get_mut(&team_1_name).unwrap();
+            value.goals_scored += team_1_score;
+            value.goals_conceded += team_2_score;
+        } else {
+            scores.insert(
+                team_1_name,
+                Team {
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score,
+                },
+            );
+        };
+        if scores.contains_key(&team_2_name) {
+            let value = scores.get_mut(&team_2_name).unwrap();
+            value.goals_scored += team_2_score;
+            value.goals_conceded += team_1_score;
+        } else {
+            scores.insert(
+                team_2_name,
+                Team {
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                },
+            );
+        };
+
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
