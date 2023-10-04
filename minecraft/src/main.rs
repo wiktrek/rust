5fbcb2883297fa2520514c769df4a0cfe9841529
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-04 21:49:06 +0200

    nethre

--- minecraft/src/main.rs
+++ minecraft/src/main.rs
@@ -1,6 +1,6 @@
 use dialoguer::{theme::ColorfulTheme, FuzzySelect, Input};
 fn main() {
-    let options = ["Hopper clock", "Nether"];
+    let options = ["Hopper clock", "Coords"];
     let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("minecraft")
         .default(0)
@@ -14,18 +14,30 @@ fn main() {
     }
 }
 fn nether() {
-    let x: f32 = Input::with_theme(&ColorfulTheme::default())
+    let x: i32 = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("x")
         .interact_text()
         .unwrap();
-    let y: f32 = Input::with_theme(&ColorfulTheme::default())
+    let y: i32 = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("y")
         .interact_text()
         .unwrap();
-    let z: f32 = Input::with_theme(&ColorfulTheme::default())
+    let z: i32 = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("z")
         .interact_text()
         .unwrap();
+    let options = ["Overworld -> Nether", "Nether -> Overworld"];
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .with_prompt("minecraft")
+        .default(0)
+        .items(&options[..])
+        .interact()
+        .unwrap();
+    match selection {
+        0 => println!("x:{}\ny:{}\nz:{}", (x / 8), y, (z / 8)),
+        1 => println!("x:{}\ny:{}\nz:{}", (x * 8), y, (z * 8)),
+        _ => println!("Error"),
+    }
 }
 fn hopper_clock() {
     let blocks: f32 = Input::with_theme(&ColorfulTheme::default())
