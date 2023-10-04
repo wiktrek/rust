a2689d08302849be15600b19e80750011a0f13eb
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-04 21:44:54 +0200

    Select

--- minecraft/src/main.rs
+++ minecraft/src/main.rs
@@ -1,17 +1,33 @@
-use dialoguer::{
-    theme::ColorfulTheme,
-    // FuzzySelect
-    Input,
-};
+use dialoguer::{theme::ColorfulTheme, FuzzySelect, Input};
 fn main() {
-    // let options = vec!["Programming", "Reading", "Learning rust", "Other"];
-    // let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
-    //     .with_prompt("what is your hobby")
-    //     .default(0)
-    //     .items(&options[..])
-    //     .interact()
-    //     .unwrap();
-    println!("hopper clock");
+    let options = ["Hopper clock", "Nether"];
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .with_prompt("minecraft")
+        .default(0)
+        .items(&options[..])
+        .interact()
+        .unwrap();
+    match selection {
+        0 => hopper_clock(),
+        1 => nether(),
+        _ => println!("No option"),
+    }
+}
+fn nether() {
+    let x: f32 = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("x")
+        .interact_text()
+        .unwrap();
+    let y: f32 = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("y")
+        .interact_text()
+        .unwrap();
+    let z: f32 = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("z")
+        .interact_text()
+        .unwrap();
+}
+fn hopper_clock() {
     let blocks: f32 = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Blocks")
         .interact_text()
