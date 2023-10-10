d0e2a187bcc3446cf466b1c5187eb0c46c5a4974
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-10 18:57:42 +0200

    from_into.rs

--- menu_test/src/main.rs
+++ menu_test/src/main.rs
@@ -2,7 +2,7 @@ use dialoguer::{theme::ColorfulTheme, FuzzySelect, Input};
 use std::fs;
 use std::path::Path;
 fn main() {
-    let options = vec!["Programming", "Reading", "Learning rust", "Other"];
+    let options = ["Programming", "Reading", "Learning rust", "Other"];
     let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("what is your hobby")
         .default(0)
--- rustlings/exercises/conversions/from_into.rs
+++ rustlings/exercises/conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let split: Vec<&str> = s.split(',').collect();
+        println!("{:?}", split);
+        if split.len() < 2 {
+            return Person::default();
+        }
+        let name = split[0];
+        let age = match split[1].parse::<usize>() {
+            Ok(val) => val,
+            Err(e) => {
+                println!("{:?}", e);
+                return Person::default();
+            }
+        };
+
+        println!("{}{}", age, name);
+        if name == "" {
+            return Person::default();
+        }
+        Person {
+            name: name.to_string(),
+            age: age,
+        }
     }
 }
 
