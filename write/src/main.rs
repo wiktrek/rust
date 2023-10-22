8b27e991efdc7f352df4e33dd10321d8aab9b473
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-22 23:07:49 +0200

    get random text✅

--- write/src/main.rs
+++ write/src/main.rs
@@ -4,14 +4,26 @@ use std::io;
 use std::io::prelude::*;
 fn main() {
     let stdin = io::stdin();
-    let file = fs::read_to_string("./text.json")
-        .expect("err")
-        .replace(['[', ']'], "")
-        .replace('"', "");
-    let num = rand::thread_rng().gen_range(0..100);
-    println!("{}", num);
-    println!("{}", file);
+    let file = trim_whitespaces(
+        fs::read_to_string("./text.json")
+            .expect("err")
+            .replace(['[', ']', '"', '\n', '\r'], ""),
+    );
+    let text: Vec<&str> = file.split(", ").collect();
+
+    println!("{:?}", text);
+
+    let num = rand::thread_rng().gen_range(0..text.len());
+    println!("{} ", text[num]);
     for line in stdin.lock().lines() {
-        println!("{}", line.unwrap());
+        if line.unwrap() == text[num] {
+            println!("✅")
+        } else {
+            println!("❌")
+        }
     }
+}
+fn trim_whitespaces(s: String) -> String {
+    let words: Vec<_> = s.split_whitespace().collect();
+    words.join(" ")
 }
