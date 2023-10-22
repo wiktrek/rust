b62d512d76c7e535d188a8913bf2ecafd29eab8d
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-22 22:57:09 +0200

    text.json

--- write/src/main.rs
+++ write/src/main.rs
@@ -1,9 +1,15 @@
+use rand::Rng;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
 fn main() {
     let stdin = io::stdin();
-    let file = fs::read_to_string("./text.json").expect("err");
+    let file = fs::read_to_string("./text.json")
+        .expect("err")
+        .replace(['[', ']'], "")
+        .replace('"', "");
+    let num = rand::thread_rng().gen_range(0..100);
+    println!("{}", num);
     println!("{}", file);
     for line in stdin.lock().lines() {
         println!("{}", line.unwrap());
