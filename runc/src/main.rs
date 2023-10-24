780812ba8104fb75b710966f60430c85018f2861
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-24 22:22:30 +0200

    It runs!"

--- runc/src/main.rs
+++ runc/src/main.rs
@@ -7,10 +7,15 @@ use std::process::Command;
 fn main() {
     println!("Hello, world!");
     let filename = "main";
-    let _output = Command::new("gcc")
-        .current_dir("./")
-        .arg(format!("c/{}.c -o {}", filename, filename))
+    let _gcc = Command::new("cmd")
+        .args(["/C", &format!("gcc c/{}.c -o {}", filename, filename)])
         .spawn()
-        .expect("Failed to execute command");
-    println!("Run {}.exe", filename)
+        .expect("failed to execute process");
+
+    println!("Running {}.exe", filename);
+
+    let _run = Command::new("cmd")
+        .args(["/C", &format!("{filename}.exe")])
+        .spawn()
+        .expect("failed to execute process");
 }
