74c5aa375978cfa5278178bfe50ba0d451413c59
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-24 22:04:13 +0200

    It runs

--- runc/src/main.rs
+++ runc/src/main.rs
@@ -2,6 +2,13 @@
     run gcc c/main.c -o main
     run main.exe
 */
+
+use std::process::Command;
 fn main() {
     println!("Hello, world!");
+    let output = Command::new("gcc")
+        .current_dir("../c")
+        .arg("c/main.c -o main")
+        .spawn()
+        .expect("Failed to execute command");
 }
