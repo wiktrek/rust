960a89faa88c130f87971fcb55d48a76d6b8b493
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-30 20:20:32 +0100

    IT WORKS

--- runc/c/main.c
+++ runc/c/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-    printf("Hello world!");
-    printf("main.c");
+    printf("Hello world!\n");
+    printf("main.c\n");
     return 0;
 }
--- runc/src/main.rs
+++ runc/src/main.rs
@@ -1,18 +1,21 @@
-
-
 use std::process::Command;
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
     let filename = "main";
-    let _gcc = Command::new("cmd")
+
+    let mut command = Command::new("cmd");
+    if let Ok(mut child) = command
         .args(["/C", &format!("gcc c/{}.c -o {}", filename, filename)])
         .spawn()
-        .expect("failed to execute process");
+    {
+        child.wait().expect("command wasn't running");
 
-    println!("Running {}.exe", filename);
-
-    let _run = Command::new("cmd")
-        .args(["/C", &format!("{filename}.exe")])
-        .spawn()
-        .expect("failed to execute process");
+        println!("Running {}.exe", filename);
+        let _run = Command::new("cmd")
+            .args(["/C", &format!("{filename}.exe")])
+            .spawn()
+            .expect("failed to execute process");
+    } else {
+        println!("ls command didn't start");
+    }
 }
