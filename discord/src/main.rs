90d4f8359d90d4437d6ed09fd6eb32206cae934d
wiktrek <wiktor.janiszewski10@gmail.com>
2023-12-20 22:12:00 +0100

    discordbot feat: ping command

---
+++ discord/src/main.rs
@@ -0,0 +1,40 @@
+use dotenv::dotenv;
+use serenity::async_trait;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{CommandResult, Configuration, StandardFramework};
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let framework = StandardFramework::new().group(&GENERAL_GROUP);
+    framework.configure(Configuration::new().prefix("w!")); // set the bot's prefix to "~"
+
+    let token = std::env::var("TOKEN").expect("TOKEN must be set.");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
