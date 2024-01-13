ec2e290582b100c24bb962e2a886939b1f395c2a
wiktrek <wiktor.janiszewski10@gmail.com>
2024-01-14 00:39:42 +0100

    feat: cm to inch

--- inchcm/src/main.rs
+++ inchcm/src/main.rs
@@ -1,6 +1,35 @@
-// 5′0″ 5′6″
+use dialoguer::{theme::ColorfulTheme, FuzzySelect};
+use std::io::{stdin, stdout, Write};
 fn main() {
-    use std::io::{stdin, stdout, Write};
+    let options = ["Inches to centimeters", "Centimeters to inches"];
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select option")
+        .default(0)
+        .items(&options[..])
+        .interact()
+        .unwrap();
+
+    match selection {
+        0 => inches_to_centimeters(),
+        1 => centimeters_to_inches(),
+        _ => (),
+    }
+}
+fn centimeters_to_inches() {
+    let mut s = String::new();
+    print!("cm: ");
+    let _ = stdout().flush();
+    stdin()
+        .read_line(&mut s)
+        .expect("Did not enter a correct string");
+    let inches = s.replace(['\r', '\n'], "").parse::<f32>().unwrap() / 2.54;
+    let feet = (inches / 12.0) as i32;
+    println!(
+        "{inches}inch \n{feet} feet and {}inches",
+        inches as i32 - feet * 12
+    );
+}
+fn inches_to_centimeters() {
     let mut s = String::new();
     print!("feet: ");
     let _ = stdout().flush();
