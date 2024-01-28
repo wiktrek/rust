2140fe8e3aa0874d5813fea21ec068fb64142cab
wiktrek <wiktor.janiszewski10@gmail.com>
2024-01-28 15:05:44 +0100

    feat: html

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,21 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{get, http::StatusCode, web, App, HttpResponse, HttpServer, Result};
+use open::that;
 
-// this function could be located in a different module
+#[get("/hi/{name}")] // <- define path parameters
+async fn hi(path: web::Path<String>) -> Result<String> {
+    let name = path.into_inner();
+    Ok(format!("Hi {}", name))
+}
+
+#[get("/")]
+async fn index() -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("index.html")))
+}
+
 fn scoped_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/test")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("Works!") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
+    cfg.service(hi);
 }
 
 // this function could be located in a different module
@@ -20,14 +29,13 @@ fn config(cfg: &mut web::ServiceConfig) {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Starting server at http://127.0.0.1:8080");
+    let _ = that("http://127.0.0.1:8080");
     HttpServer::new(|| {
         App::new()
             .configure(config)
             .service(web::scope("/api").configure(scoped_config))
-            .route(
-                "/",
-                web::get().to(|| async { HttpResponse::Ok().body("/") }),
-            )
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
