5e86515f3f851a9757cb0825b22198a479683fcc
wiktor <wiktor.janiszewski10@gmail.com>
2024-02-23 23:47:10 +0100

    feat: eating system

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,4 +1,5 @@
 //! Shows how to render simple primitive shapes with a single color.
+
 #[cfg(feature = "debug")]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy::{
@@ -13,7 +14,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .init_state::<Direction>()
         .add_systems(Startup, setup)
-        .add_systems(Update, (move_snake, update_direction, check_if_lost_or_ate));
+        .add_systems(Update, (move_snake, update_direction, eating_system));
     #[cfg(feature = "debug")]
     app.add_plugins(WorldInspectorPlugin::new());
     app.run()
@@ -27,27 +28,37 @@ enum Direction {
     Right
 }
 #[derive(Component)]
+pub struct Score {
+    pub value: usize
+}
+#[derive(Component)]
 struct SnakeHead;
 #[derive(Component)]
 struct SnakeTail;
 
 #[derive(Component)]
 struct Food;
+
+#[derive(Default)]
+pub struct SnakeParts(pub Vec<Entity>);
+
+pub const PLAYER: f32 = 30.;
+pub const FOOD_DIMENSIONS: f32 = 10.;
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>, 
+    mut materials: ResMut<Assets<ColorMaterial>>
 ) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn((MaterialMesh2dBundle {
-        mesh: meshes.add(Rectangle::new(30.0, 30.0)).into(),
+        mesh: meshes.add(Rectangle::new(PLAYER, PLAYER)).into(),
         transform: Transform::from_translation(Vec3::new(-96., 0., 0.)),
         material: materials.add(ColorMaterial::default()),
         ..default()
-    }, SnakeHead));
+    }, SnakeHead)).insert(Score { value: 0});
     commands.spawn((MaterialMesh2dBundle {
-        mesh: meshes.add(Rectangle::new(30.0, 30.0)).into(),
+        mesh: meshes.add(Rectangle::new(PLAYER, PLAYER)).into(),
         transform: Transform::from_translation(Vec3::new(-100., 0., 0.)),
         material: materials.add(ColorMaterial::default()),
         ..default()
@@ -56,12 +67,12 @@ fn setup(
 }
 fn move_snake(time: Res<Time>, mut snake: Query<&mut Transform, With<SnakeHead>>,direction: Res<State<Direction>>,) {
     for mut transform in &mut snake {
-    //     match **direction {
-    //         Direction::Up => transform.translation.y += 50. * time.delta_seconds(),
-    //         Direction::Down => transform.translation.y -= 50. * time.delta_seconds(),
-    //         Direction::Left => transform.translation.x -= 50. * time.delta_seconds(),
-    //         Direction::Right => transform.translation.x += 50. * time.delta_seconds(),
-    // }
+        match **direction {
+            Direction::Up => transform.translation.y += 50. * time.delta_seconds(),
+            Direction::Down => transform.translation.y -= 50. * time.delta_seconds(),
+            Direction::Left => transform.translation.x -= 50. * time.delta_seconds(),
+            Direction::Right => transform.translation.x += 50. * time.delta_seconds(),
+    }
 }
 }
 fn update_direction(mut direction: ResMut<NextState<Direction>>, keyboard: Res<ButtonInput<KeyCode>>,) {
@@ -83,7 +94,7 @@ fn spawn_apple(
 ) {
     commands.spawn(
         (MaterialMesh2dBundle {
-        mesh: meshes.add(Rectangle::new(10.0, 10.0)).into(),
+        mesh: meshes.add(Rectangle::new(FOOD_DIMENSIONS, FOOD_DIMENSIONS)).into(),
         transform:  Transform::from_xyz(
                 10.0,
                 0.0,
@@ -95,40 +106,28 @@ fn spawn_apple(
     Food
 ));
 }
-fn check_if_lost_or_ate(
+fn eating_system(
     mut set: ParamSet<(
-        Query<&mut Transform, With<SnakeHead>>,
-        Query<&mut Transform, With<Food>>,
-        Query<&mut Transform, With<SnakeTail>>,
-    )>,
+        Query<(&mut Score ,&mut Transform), With<SnakeHead>>,
+        Query<(Entity, &mut Transform), With<Food>>,
+    )>, mut commands: Commands,
 ) {
     let head_position = {
         let p0 = set.p0();
-        let head = p0.get_single().unwrap();
+        let head = p0.get_single().unwrap().1;
         Vec2::new(head.translation.x, head.translation.y)
     };
-
-    let tail_positions: Vec<Vec2> = set
-        .p2()
-        .iter_mut()
-        .map(|t| Vec2::new(t.translation.x, t.translation.y))
-        .collect();
-
-    for &tail_position in &tail_positions {
-        if head_position.x == tail_position.x && head_position.y == tail_position.y {
-            println!("Game Over: Snake collided with itself!");
-            // Handle game over logic here
-            return;
-        }
-    }
-
-    // Separate block for the second mutable borrow
     {
-        let p1 = set.p1();
-        let food = p1.get_single().unwrap();
-        if head_position.x == food.translation.x && head_position.y == food.translation.y {
-            println!("Snake ate the food!");
-            // Handle food eating logic here
-        }
+        for (entity, food) in set.p1().iter_mut() {
+         if ((((food.translation.x - head_position.x) as f32).powf(2.0) + ((food.translation.y - head_position.y) as f32).powf(2.0)) as f32).sqrt() < (PLAYER / 2.0 + FOOD_DIMENSIONS / 2.0) {
+            let e = commands.get_entity(entity);
+            if e.is_some() {
+                let mut unwraped = e.unwrap();
+                println!("{:?}", unwraped.id());
+                unwraped.despawn()
+            }
+            // set.p0().get_single_mut().unwrap().0.value += 1;
+            // println!("SNAKE ATE {}", set.p0().get_single().unwrap().0.value )
+    }}
     }
 }
